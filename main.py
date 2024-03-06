8fbf045699394c7373773f36f05914ef384330b5
Lucas Marques <lucaas.gaabriel@live.com>
2024-03-06 09:39:23 -0300

    Feat: main function para execução

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from functions import *
 import pyautogui as pg
 import time
 
-iniciar()
-time.sleep(1.5)
-res970_90()
-time.sleep(1)
 
-fecha_campanha = pg.locateOnScreen('dropdown.png')
+def main():
+    iniciar()
+    time.sleep(2.5)
+    res970_90()
+    time.sleep(1)
 
-if fecha_campanha:
-    x, y = pg.center(fecha_campanha)
-    pg.click(x, y)
+    fecha_campanha = pg.locateOnScreen('dropdown.png')
 
-res1248_250()
-pg.hotkey('alt','f4')
+    if fecha_campanha:
+        x, y = pg.center(fecha_campanha)
+        pg.click(x, y)
+
+    res1248_250()
+    pg.hotkey('alt','f4')
+
+if __name__ == '__main__':
+    main()
